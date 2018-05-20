44b3fa385a666f95f45f0b6cc1338ac3c7077883
Lucyna Mazur <lucyna@studiokreator.com>
2018-05-20 09:42:19 +0200

    saving xml data into db

--- JPK_app/views.py
+++ JPK_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views import View
+from .models import SprzedazWiersz
+import xml.etree.ElementTree as etree
 
 # Create your views here.
+
+
+
+tree = etree.parse('JPK/JPK_VAT2_20171015_191844_00c.xml')
+root = tree.getroot()
+
+class SprzedazView(View):
+    def get(self, request):
+        sprzedaz = {}
+        for row in root.findall('{http://jpk.mf.gov.pl/wzor/2016/10/26/10261/}SprzedazWiersz'):
+            for element in row:
+                sprzedaz[element.tag[element.tag.index('}')+1:]] = element.text
+            SprzedazWiersz.objects.create(**sprzedaz)
+            sprzedaz.clear()
+        ctx = {'sprzedaz': sprzedaz}
+        return render(request, 'sprzedaz.html', ctx)
+
+#
--- JPK_django/urls.py
+++ JPK_django/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from JPK_app.views import SprzedazView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^sprzedaz/$', SprzedazView.as_view(), name='sprzedaz'),
 ]
