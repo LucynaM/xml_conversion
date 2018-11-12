bc805949a57dcb8f0b741fc830f9f52f801c528d
Lucyna Mazur <lucyna@studiokreator.com>
2018-11-12 10:30:59 +0100

    correct way of selecting tag fields according to their type

--- JPK_app/prepare_tags_scheme.py
+++ JPK_app/prepare_tags_scheme.py
@@ -9,15 +9,19 @@ def prepare_tags_scheme(ns):
         if jpk_file.ns == ns:
             obj = jpk_file
 
+    #represent file content as dictionary where key is a table name and values are tags names
     obj.tags = {}
+    #select all tags that should contain dates
     obj.date_fields = []
+    # select all tags that should contain money value
     obj.money_fields = []
+    # select all tags that should contain numbers
     obj.quantity_fields = []
 
     for table in obj.tables.all():
         obj.tags[table.name] = [tag.name for tag in table.tags.all()]
-        obj.date_fields = [tag.name for tag in table.tags.filter(type="date")]
-        obj.money_fields = [tag.name for tag in table.tags.filter(type="value")]
-        obj.quantity_fields = [tag.name for tag in table.tags.filter(type="num")]
+        [obj.date_fields.append(tag.name) for tag in table.tags.filter(type="date")]
+        [obj.money_fields.append(tag.name) for tag in table.tags.filter(type="value")]
+        [obj.quantity_fields.append(tag.name) for tag in table.tags.filter(type="num")]
     return obj
 
